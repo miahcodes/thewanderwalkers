ee8375ed1cac75e023be4c66aa6d0fb8a8dc4cf9
Miah Abalos <32051471+miahcodes@users.noreply.github.com>
2019-03-12 09:27:03 +0800

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+(function($) {
+
+$('.card-container.mobile').flickity({
+	autoPlay:'true',
+	adaptiveHeight: 'true'
+});
+$('.open a').click(function(e){
+	e.preventDefault();
+	var $this = $('.main-nav').slideToggle();
+	console.log($this);
+});
+$(window).scroll(function(event) {
+			let $navBg = $('.masthead');
+			if($(window).scrollTop() > 20){
+				$navBg.addClass('active');
+			} else {
+				
+				$navBg.removeClass('active');
+			}
+			
+		});
+
+$('.close-btn').click(function(e){
+	$('#popin-container').fadeOut();
+});
+
+})(jQuery);
